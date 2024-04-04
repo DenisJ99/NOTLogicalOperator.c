e98f3aac91d27d30493bebae5c02b484f8e564da
Denis Johnson <166014361+DenisJ99@users.noreply.github.com>
2024-04-04 17:00:17 +0100

    Add files via upload

---
+++ NOTLogicalOperator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    bool sunny = true;
+
+    if(!sunny){
+        printf("\nIt's cloudy outside!");
+    }
+    else{
+        printf("\nIt's sunny outside!");
+    }
+
+    return 0;
+}
+
+/* Logical operators = ! (NOT) reverses the state of a condition
+*/
